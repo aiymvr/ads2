cb7f6c4e752f68addd0694a9adbddd62365bddf6
Админ <241656@astanait.edu.kz>
2025-04-09 23:44:54 +0500

    Add MyQueue with MyLinkedList

---
+++ src/MyQueue.java
@@ -0,0 +1,30 @@
+public class MyQueue<T> {
+    private MyLinkedList<T> list;
+
+    public MyQueue(){
+        list = new MyLinkedList<>();
+    }
+
+    public int size(){
+        return list.size();
+    }
+    public boolean empty(){
+        return list.size() ==0;
+    }
+    public T peek(){
+        return list.get(0);
+    }
+
+    public T enqueue(T item){
+        list.addLast(item);
+        return item;
+
+    }
+
+    public T dequeue(){
+        T removingItem = peek();
+        list.removeFirst();
+        return removingItem;
+    }
+
+}
