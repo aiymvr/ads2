7a1c4e04d167a717887cb2ae4d4677c43cc7e83f
aiymvr <241656@astanait.edu.kz>
2025-04-12 22:15:34 +0500

    Update MyStack.java

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,39 +1,72 @@
+/**
+ * MyStack is a custom implementation of a Stack data structure.
+ * The stack is implemented using MyArrayList as the underlying storage.
+ *
+ * @param <T> the type of elements in the stack
+ */
 public class MyStack<T> {
-    private MyArrayList<T> list;
-
-    public MyStack(){
+    private MyArrayList<T> list; 
+    /**
+     * Constructor to initialize an empty stack.
+     */
+    public MyStack() {
         list = new MyArrayList<>();
     }
 
-    public T push(T newItem){
-        list.add(newItem);
+    /**
+     * Adds a new element to the top of the stack.
+     *
+     * @param newItem the element to add
+     * @return the added element
+     */
+    public T push(T newItem) {
+        list.add(newItem); 
         return newItem;
     }
 
-    public  boolean empty(){
+    /**
+     * Checks if the stack is empty.
+     *
+     * @return true if the stack is empty, false otherwise
+     */
+    public boolean empty() {
         return list.size() == 0;
     }
-    public int size(){
+
+    /**
+     * Returns the number of elements in the stack.
+     *
+     * @return the size of the stack
+     */
+    public int size() {
         return list.size();
     }
 
-    public T pop(){
-        if(empty()){
-            throw new IllegalStateException("error");
+    /**
+     * Removes and returns the top element of the stack.
+     *
+     * @return the top element of the stack
+     * @throws IllegalStateException if the stack is empty
+     */
+    public T pop() {
+        if (empty()) {
+            throw new IllegalStateException("Stack is empty");
         }
-        T top = list.get(list.size()-1);
-        list.remove(list.size()-1);
+        T top = list.get(list.size() - 1); 
+        list.remove(list.size() - 1); 
         return top;
     }
 
-    public T peek(){
-        if(empty()){
-            throw new IllegalStateException("error");
+    /**
+     * Retrieves the top element of the stack without removing it.
+     *
+     * @return the top element of the stack
+     * @throws IllegalStateException if the stack is empty
+     */
+    public T peek() {
+        if (empty()) {
+            throw new IllegalStateException("Stack is empty");
         }
-        T top = list.get(list.size()-1);
-        return top;
-
+        return list.get(list.size() - 1); 
     }
-
-
 }
