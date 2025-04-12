c162bf9fbc84ec997090fd03cc28ba7eea52288e
aiymvr <241656@astanait.edu.kz>
2025-04-12 22:10:34 +0500

    Update MyQueue.java

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,30 +1,71 @@
-public class MyQueue<T extends Comparable<T>>{
-    private MyLinkedList<T> list;
+/**
+ * MyQueue is a custom implementation of a Queue data structure.
+ * The queue is implemented using MyLinkedList as the underlying storage.
+ *
+ * @param <T> the type of elements in the queue, which must implement Comparable
+ */
+public class MyQueue<T extends Comparable<T>> {
+    private MyLinkedList<T> list; 
 
-    public MyQueue(){
+    
+    /**
+     * Constructor to initialize an empty queue.
+     */
+    public MyQueue() {
         list = new MyLinkedList<>();
     }
 
-    public int size(){
+    /**
+     * Returns the number of elements in the queue.
+     *
+     * @return the size of the queue
+     */
+    public int size() {
         return list.size();
     }
-    public boolean empty(){
-        return list.size() ==0;
+
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return true if the queue is empty, false otherwise
+     */
+    public boolean empty() {
+        return list.size() == 0;
     }
-    public T peek(){
+
+    /**
+     * Retrieves the front element of the queue without removing it.
+     *
+     * @return the front element of the queue
+     * @throws IllegalStateException if the queue is empty
+     */
+    public T peek() {
+        if (empty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
         return list.get(0);
     }
 
-    public T enqueue(T item){
-        list.addLast(item);
+    /**
+     * Adds a new element to the end of the queue.
+     *
+     * @param item the element to add
+     * @return the added element
+     */
+    public T enqueue(T item) {
+        list.addLast(item); 
         return item;
-
     }
 
-    public T dequeue(){
-        T removingItem = peek();
+    /**
+     * Removes and returns the front element of the queue.
+     *
+     * @return the front element of the queue
+     * @throws IllegalStateException if the queue is empty
+     */
+    public T dequeue() {
+        T removingItem = peek();t
         list.removeFirst();
         return removingItem;
     }
-
 }
